d87957d5f508883bb92ec9045e2897c9a9e6e499
Yaswanth1630 <hyaswanth8@gmail.com>
2026-05-20 17:18:39 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,12 +1,13 @@
 public class Student {
     public static void main(String[] args) {
         int rollno = 1596;
-        String sname = "KRISHNA";
+        String sname = "yash";
         String course = "PYTHON"; 
         System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("Thanks for using My Application");
+        System.out.println("Thanks for learning");
     }
 }
