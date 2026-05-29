d7ef88ffc1aae3faa71c58fd4370b4d2db6ed9fe
Yaswanth1630 <hyaswanth8@gmail.com>
2026-05-29 12:04:29 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -8,7 +8,6 @@ public class Student {
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("Thanks for using My Application");
-        System.out.println("Thanks for teaching yash ");
-        
+        System.out.println("Thanks for teaching yashuu ");   
     }
 }
