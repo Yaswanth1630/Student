01cf0023d4656f5d19b7d985c6caa55451c5b4dd
Yaswanth1630 <hyaswanth8@gmail.com>
2026-06-07 10:29:52 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -7,6 +7,7 @@ public class Student {
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
-        System.out.println("Thanks for using My Application");   
+        System.out.println("Thanks for using My Application");
+        System.out.println("I'm learning Devops");
     }
 }
