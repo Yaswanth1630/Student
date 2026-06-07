4829c71ef892c3917c0ea01eb3c6dd6a6e9a8e09
Yaswanth1630 <hyaswanth8@gmail.com>
2026-06-07 10:40:33 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -8,6 +8,6 @@ public class Student {
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("Thanks for using My Application");
-        System.out.println("I'm learning Devops");
+        System.out.println("I'm learning how to integrate jenkins aand adding webhook");
     }
 }
